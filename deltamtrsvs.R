31f5f60d5844f928888d63d6def3628ec77571fb
Eric Youngson <eric@scneco.com>
2015-12-22 19:07:44 -0800

    Moved body of first function in R script into a loop to process all files whose names are passed as a list in the argument

--- deltamtrsvs.R
+++ deltamtrsvs.R
@@ -6,11 +6,11 @@
 # This file contains functions for working with the results of API queries from
 # DeltaMeterServices.com
 
-install.packages("lubridate")
+install.packages("lubridate") ## Comment out after first calsource()
 library("lubridate")
 
 
-DmsAuditFormat <- function(fname){
+DmsAuditFormat <- function(fnames){
   # Reformats date-time strings to R date type objects: removing empty time
   # values, then reorders dataframe rows by absolute month (without year).
   #
@@ -20,16 +20,16 @@ DmsAuditFormat <- function(fname){
   # Returns:
   #   a dataframe containing the original file content reordered by absolute
   #   month (regardless of year)
-  audit.data = read.csv(fname, header = TRUE, stringsAsFactors = FALSE)
-  
-  audit.data$Per..Start = as.Date(audit.data$Per..Start)
-  audit.data$Per..End = as.Date(audit.data$Per..End)
-  audit.data$Per..Start.1 = as.Date(audit.data$Per..Start.1)
-  audit.data$Per..End.1 = as.Date(audit.data$Per..End.1)
+  for(i in 1:length(fnames)){
+    audit.data = read.csv(fnames[i], header = TRUE, stringsAsFactors = FALSE)
+    
+    audit.data$Per..Start = as.Date(audit.data$Per..Start)
+    audit.data$Per..End = as.Date(audit.data$Per..End)
+    audit.data$Per..Start.1 = as.Date(audit.data$Per..Start.1)
+    audit.data$Per..End.1 = as.Date(audit.data$Per..End.1)
 
-  audit.data = audit.data[order(month(audit.data$Per..Start)),]
-
-  print(mnths)
-  write.csv(audit.data, paste("out-", fname))
+    audit.data = audit.data[order(month(audit.data$Per..Start)),]
+    write.csv(audit.data, paste("out-", fnames[i]))
+  }
   return(audit.data)
 }
