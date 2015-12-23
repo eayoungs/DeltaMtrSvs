0c4be495c3fca6c57a3848abb91d076da22302f4
Eric Youngson <eric@scneco.com>
2015-12-22 16:20:01 -0800

    Added module header to R script file

--- deltamtrsvs.R
+++ deltamtrsvs.R
@@ -1,6 +1,24 @@
+__author__ = "Eric Allen Youngson"
+__email__ = "eric@scneco.com"
+__copyright__ = "Copyright 2015, Succession Ecological Services"
+__license__ = "GNU Affero (GPLv3)"
+
+# This file contains functions for working with the results of API queries from
+# DeltaMeterServices.com
+
+library("lubridate")
+
+
 dms.adt.vis <- function(fname){
-  library("lubridate")
-  
+  # Reorders audit data from DeltaMeterServices.com for review, based on
+  # requirements of the America Saves\! project
+  #
+  # Args:
+  #   fname: File name to be processed by function
+  # 
+  # Returns:
+  #   a dataframe containing the original file content reordered by absolute
+  #   month (regardless of year)
   adt.data = read.csv(fname, header = TRUE, stringsAsFactors = FALSE)
   
   mnths = data.frame()
