0a407bc0b4d71023f0fd28399048c0974be86efd
Eric Youngson <eric@scneco.com>
2016-02-18 12:01:18 -0800

    Revised function naming scheme, revised parameter name in first function, added docstring to last function & revised elsewhere

--- deltamtrsvs.R
+++ deltamtrsvs.R
@@ -11,25 +11,25 @@ library("lubridate")
 # install.packages("ggplot2") ## Comment out after first call to source()
 library("ggplot2")
 # install.packages("Rmisc") ## Comment out after first call to source()
-# library("Rmisc")
+library("Rmisc")
 
 
-DmsAuditFormat <- function(fnames){
+DmsFmatAudit <- function(f.names){
   # Reformats date-time strings to R date type objects: removing empty time
   # values, then reorders dataframe rows by absolute month (without year).
   #
   # Args:
-  #   fnames: File names, as a list, to be processed by function
+  #   f.names: File names, as a list, to be processed by function
   # 
   # Returns:
   #   A dataframe containing the original file content reordered by absolute
   #   month (regardless of year), and contents of each dataframe to a seperate
   #   file.
   
-  # fnames = c(read.table(fname_list, sep="\n", stringsAsFactors = FALSE))
+  # f.names = c(read.table(fname_list, sep="\n", stringsAsFactors = FALSE))
   audit.data.lst = list()
-  for(i in 1:length(fnames)){
-    audit.data = read.csv(fnames[i], header = TRUE, stringsAsFactors = FALSE)
+  for(i in 1:length(f.names)){
+    audit.data = read.csv(f.names[i], header = TRUE, stringsAsFactors = FALSE)
     
     audit.data$Per..Start = as.Date(audit.data$Per..Start)
     audit.data$Per..End = as.Date(audit.data$Per..End)
@@ -38,20 +38,20 @@ DmsAuditFormat <- function(fnames){
 
     audit.data = audit.data[order(month(audit.data$Per..Start.1)),]
     audit.data.lst[[i]] <- audit.data
-    # write.csv(audit.data, paste("out-", fnames[i]))
+    # write.csv(audit.data, paste("out-", f.names[i]))
   audit.data = data.frame(audit.data.lst[1])
   }
   return(audit.data)
 }
 
-UsePerDmsPlot <- function(df.name){
-  # Plots a data-frame containing usage data from the DeltaMeterServices.com
+DmsPlotUsePer <- function(df.name){
+  # Plots a data-frame containing *audit* data from the DeltaMeterServices.com
   # website; power = f(reading length)
   #
   # Args:
-  #   
+  #   df.name: 
   # Returns:
-  #   
+  #   pwr.tmp.mplot: 
   audit.data = read.csv(df.name, header = TRUE, stringsAsFactors = FALSE)
   pwr.tmp.gas = qplot(audit.data[,"Hrs..in.Per."], audit.data[,"X.W.SF."], 
                       colour = df.name, main = "Gas", xlab = "Hrs. In Period",
@@ -65,7 +65,16 @@ UsePerDmsPlot <- function(df.name){
   return(pwr.tmp.mplot)
 }
 
-UseRange <- function(df.name){
+DmsGetUseRange <- function(df.name){
+  # Takes a data-frame of 'audit' data from DeltaMeterServices.com as argument,
+  # returns the start & end dates for both (Electric & Gas) common fuel types.
+  #
+  # Args:
+  #   df.name: Data-frame containing utility billing history from
+  #   DeltaMeterServices.com
+  # Returns:
+  #   use.range: Data-frame containing the start & end dates for both (Electric
+  #   & Gas)common fuel type
   gas.start = min(df.name$Per..Start, na.rm=TRUE)
   gas.end = max(df.name$Per..End, na.rm=TRUE)
   elec.start = min(df.name$Per..Start.1, na.rm=TRUE)
